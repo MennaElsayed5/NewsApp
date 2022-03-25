cf84581937f5a9a08e9f2ad2a117d7e96b1aa66e
radwa <radwamohamed2979@gmail.com>
2022-03-25 14:35:02 +0200

    update nullable

--- app/src/main/java/com/example/news/model/NewsApi.kt
+++ app/src/main/java/com/example/news/model/NewsApi.kt
@@ -1,28 +1,27 @@
 package com.example.news.model
 
 import androidx.room.Entity
-import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "News")
 data class NewsApi(
-    val id: String,
-    val status:String,
-    val totalResults:Int,
+    val id: String?,
+    val status: String?,
+    val totalResults: Int?,
     val articles: List<Article>
 )
 
-data class Article (
-    val source: Source,
+data class Article(
+    val source: Source?,
     val author: String?,
-    val title: String,
-    val description: String,
-    val url: String,
-    val urlToImage: String,
-    val publishedAt: String,
-    val content: String
+    val title: String?,
+    val description: String?,
+    val url: String?,
+    val urlToImage: String?,
+    val publishedAt: String?,
+    val content: String?
 )
 
-data class Source (
-    val id: String,
-    val name: String
+data class Source(
+    val id: String?,
+    val name: String?
 )
