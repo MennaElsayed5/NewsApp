c39744c3a37680bb3c115f3f808513384ecdbd6b
Menna <mennaaaaelsayed@gmail.com>
2022-03-25 13:53:56 +0200

    user pojo and create data base

--- app/src/main/java/com/example/news/model/User.kt
+++ app/src/main/java/com/example/news/model/User.kt
@@ -1,4 +1,12 @@
 package com.example.news.model
 
-class User {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Users")
+data class User(
+    @PrimaryKey
+    var email : String,
+    var name: String,
+    var phoneNumber: Int
+)
