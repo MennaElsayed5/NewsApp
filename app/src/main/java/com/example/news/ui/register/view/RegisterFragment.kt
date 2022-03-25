0804257e4682599460e31749e638777519986040
Menna <mennaaaaelsayed@gmail.com>
2022-03-25 20:52:29 +0200

    register screen without email valid

--- app/src/main/java/com/example/news/ui/register/view/RegisterFragment.kt
+++ app/src/main/java/com/example/news/ui/register/view/RegisterFragment.kt
@@ -5,14 +5,16 @@ import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.news.databinding.RegisterFragmentBinding
 import com.example.news.model.User
 import com.example.news.model.UserRepository
 import com.example.news.ui.register.view_model.RegisterViewModel
 import com.example.news.ui.register.view_model.ViewModelFactory
-import java.util.regex.Pattern
 
 
 class RegisterFragment : Fragment() {
@@ -21,6 +23,7 @@ class RegisterFragment : Fragment() {
         ViewModelFactory(UserRepository.getRepoInstance(requireActivity().application))
     }
     private var user: User = User("", "", "0", "")
+    private lateinit var navController: NavController
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -31,21 +34,40 @@ class RegisterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        user = User("menna@gmail.com", "menna", "2222", "12346")
-        viewModel.insertUser(user)
+        binding.btnSignUp.setOnClickListener {
+            if (isDataValid()) {
+                user = createUser()
+                viewModel.insertUser(user)
+                Toast.makeText(requireContext(), "register", Toast.LENGTH_LONG).show()
+                navController = Navigation.findNavController(view)
+                navController.navigate(RegisterFragmentDirections.actionRegisterFragmentToHomeFragment())
+
+            }
+        }
     }
 
-    private fun createUser(): User =
-        User(
-            email = checkEmail(binding.txtEmail.text.toString()).toString(),
-            name = binding.txtName.text.toString(),
-            phoneNumber = binding.txtPhone.text.toString(),
-            password = binding.txtPassword.text.toString()
-        )
+    private fun createUser(): User = User(
+        email = binding.txtEmail.text.toString(),
+        name = binding.txtName.text.toString(),
+        phoneNumber = binding.txtPhone.text.toString(),
+        password = binding.txtPassword.text.toString()
+    )
 
-    private fun checkEmail(email: String): Boolean {
-        val pattern: Pattern = Patterns.EMAIL_ADDRESS
-        return pattern.matcher(email).matches()
+    private fun isDataValid(): Boolean {
+        val emailRegex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$"
+        if (binding.edtEmail.editText!!.text.toString() != emailRegex) {
+            binding.edtEmail.error = "Not Valid email"
+        } else if (binding.edtName.editText!!.text.toString().isEmpty()) {
+            binding.edtName.error = "empty_name"
+        } else if (binding.edtPhoneNum.editText!!.text.toString().isEmpty()) {
+            binding.edtPhoneNum.setError("empty_phone")
+        } else if (binding.edtPassword.editText!!.text.toString().isEmpty()) {
+            binding.edtPassword.error = "empty password"
+        } else {
+            return true
+        }
+        Toast.makeText(requireContext(), "input not valid", Toast.LENGTH_LONG).show()
+        return false
     }
 
 }
