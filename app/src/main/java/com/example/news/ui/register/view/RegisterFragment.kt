b2295103776d3f819e54497deabecc03e8701af2
Menna <mennaaaaelsayed@gmail.com>
2022-03-26 09:43:24 +0200

    register screen with all valid

--- app/src/main/java/com/example/news/ui/register/view/RegisterFragment.kt
+++ app/src/main/java/com/example/news/ui/register/view/RegisterFragment.kt
@@ -19,7 +19,7 @@ import com.example.news.ui.register.view_model.ViewModelFactory
 
 class RegisterFragment : Fragment() {
     private lateinit var binding: RegisterFragmentBinding
-    private val viewModel: RegisterViewModel by viewModels<RegisterViewModel> {
+    private val viewModel: RegisterViewModel by viewModels {
         ViewModelFactory(UserRepository.getRepoInstance(requireActivity().application))
     }
     private var user: User = User("", "", "0", "")
@@ -27,20 +27,32 @@ class RegisterFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = RegisterFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel.mutableLiveData.observe(viewLifecycleOwner) {
+            it.let {
+                if (!it) {
+                    viewModel.insertUser(user)
+                    Toast.makeText(requireContext(), "register successful", Toast.LENGTH_LONG)
+                        .show()
+                    navController = Navigation.findNavController(view)
+                    navController.navigate(RegisterFragmentDirections.actionRegisterFragmentToHomeFragment())
+
+                } else {
+                    Toast.makeText(requireContext(), "User Already Existed", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+        }
         binding.btnSignUp.setOnClickListener {
             if (isDataValid()) {
                 user = createUser()
-                viewModel.insertUser(user)
-                Toast.makeText(requireContext(), "register", Toast.LENGTH_LONG).show()
-                navController = Navigation.findNavController(view)
-                navController.navigate(RegisterFragmentDirections.actionRegisterFragmentToHomeFragment())
+                viewModel.getExistUser(user.email, user.password)
 
             }
         }
@@ -54,19 +66,20 @@ class RegisterFragment : Fragment() {
     )
 
     private fun isDataValid(): Boolean {
-        val emailRegex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$"
-        if (binding.edtEmail.editText!!.text.toString() != emailRegex) {
-            binding.edtEmail.error = "Not Valid email"
+        val email = binding.txtEmail.text.toString()
+        val regex = Patterns.EMAIL_ADDRESS.matcher(email).matches()
+        if (!regex) {
+            binding.edtEmail.error = "invalid email"
         } else if (binding.edtName.editText!!.text.toString().isEmpty()) {
-            binding.edtName.error = "empty_name"
+            binding.edtName.error = "empty name"
         } else if (binding.edtPhoneNum.editText!!.text.toString().isEmpty()) {
-            binding.edtPhoneNum.setError("empty_phone")
+            binding.edtPhoneNum.error = "empty phone"
         } else if (binding.edtPassword.editText!!.text.toString().isEmpty()) {
             binding.edtPassword.error = "empty password"
         } else {
             return true
         }
-        Toast.makeText(requireContext(), "input not valid", Toast.LENGTH_LONG).show()
+        Toast.makeText(requireContext(), "inputs invalid", Toast.LENGTH_LONG).show()
         return false
     }
 
--- app/src/main/java/com/example/news/ui/register/view_model/RegisterViewModel.kt
+++ app/src/main/java/com/example/news/ui/register/view_model/RegisterViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.news.ui.register.view_model
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.news.model.User
@@ -8,9 +9,19 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class RegisterViewModel(private val userRepository: UserRepository) : ViewModel() {
+    var mutableLiveData = MutableLiveData<Boolean>()
     fun insertUser(user: User) {
         viewModelScope.launch(Dispatchers.IO) {
             userRepository.insert(user)
+        }
+    }
+
+    private var exist: Boolean = true
+    fun getExistUser(email: String, password: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            exist = userRepository.getUser(email, password)
+            mutableLiveData.postValue(exist)
+
         }
     }
 
